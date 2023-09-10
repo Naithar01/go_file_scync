1b6f6b6696e7b190ac5b4c3bdb7e0477adf54ec2
naithar01 <eeung025@gmail.com>
2023-09-10 14:12:09 +0900

    * Window Menubar 수정 * 파일 오픈에서 폴더 오픈으로 코드 수정 ( 경로 출력 이후 폴더 오픈 )

--- app.go
+++ app.go
@@ -26,73 +26,29 @@ func (a *App) startup(ctx context.Context) {
 }
 
 func (a *App) applicationMenu() *menu.Menu {
-
-	// We can use the same callback for multiple menu items
-	handleRadio := func(cbdata *menu.CallbackData) {
-		// The menu item that was selected can be accessed through the
-		// callback data
-		println("radio checked: ", cbdata.MenuItem.Label)
-	}
-
-	// We can define menu items and reuse them.
-	// Their state will stay in sync.
-	checkbox := &menu.MenuItem{
-		Label:   "Checked",
-		Type:    menu.CheckboxType,
-		Checked: true,
-		Click: func(cbdata *menu.CallbackData) {
-			fmt.Printf("checked = %v\n", cbdata.MenuItem.Checked)
-		},
-	}
-
-	// Radio groups are automatically made by adjacent radio menu items
-	radioMenu1 := menu.Radio("White", false, nil, handleRadio)
-	radioMenu2 := menu.Radio("Green", true, nil, handleRadio)
-	radioMenu3 := menu.Radio("Red", false, nil, handleRadio)
-
-	secretMenu := menu.SubMenu("Secret Menu",
-		menu.NewMenuFromItems(
-			menu.Text("test", nil, nil),
-			radioMenu1,
-			radioMenu2,
-			radioMenu3,
-			checkbox,
-		),
-	)
-
-	// Menu items can be hidden
-	secretMenu.Hidden = true
-
 	// Menus can be updated by callbacks. Updates will get reflected after
 	// calling `runtime.Menu.UpdateApplicationMenu()`
-	toggleSecretMenu := func(cbdata *menu.CallbackData) {
-		secretMenu.Hidden = !secretMenu.Hidden
-		if secretMenu.Hidden {
-			cbdata.MenuItem.Label = "Show secret menu...😉"
-		} else {
-			cbdata.MenuItem.Label = "Hide secret menu! 😲"
-		}
-		runtime.MenuUpdateApplicationMenu(a.ctx)
-	}
-
 	return menu.NewMenuFromItems(
 		menu.SubMenu("File", menu.NewMenuFromItems(
-			menu.Text("&Open", keys.CmdOrCtrl("o"), func(cbdata *menu.CallbackData) {
-				println("Open Called!")
-				filename, err := runtime.OpenFileDialog(a.ctx, runtime.OpenDialogOptions{
+			menu.Text("Open Directory", keys.CmdOrCtrl("o"), func(cbdata *menu.CallbackData) {
+				directory_path, err := runtime.OpenDirectoryDialog(a.ctx, runtime.OpenDialogOptions{
 					DefaultDirectory:           "",
 					DefaultFilename:            "",
-					Title:                      "Selct yer fil",
+					Title:                      "Selct Directory",
 					Filters:                    nil,
 					ShowHiddenFiles:            false,
 					CanCreateDirectories:       false,
 					ResolvesAliases:            false,
 					TreatPackagesAsDirectories: false,
 				})
+				if err != nil {
+					return
+				}
+
 				_, err = runtime.MessageDialog(a.ctx, runtime.MessageDialogOptions{
 					Type:          "info",
-					Title:         "Hello",
-					Message:       filename,
+					Title:         "Selected Directory",
+					Message:       directory_path,
 					Buttons:       nil,
 					DefaultButton: "",
 					CancelButton:  "",
@@ -100,50 +56,14 @@ func (a *App) applicationMenu() *menu.Menu {
 				if err != nil {
 					return
 				}
-				if err != nil {
-					return
-				}
+
+				runtime.BrowserOpenURL(a.ctx, directory_path)
 			}),
-			menu.Separator(),
-			menu.SubMenu("Submenu",
-				menu.NewMenuFromItems(
-					&menu.MenuItem{
-						Label:    "Disabled",
-						Type:     menu.TextType,
-						Disabled: true,
-					},
-					&menu.MenuItem{
-						Label:  "Hidden",
-						Hidden: true,
-						Type:   menu.TextType,
-					},
-					checkbox,
-					checkbox,
-					checkbox,
-					radioMenu1,
-					radioMenu2,
-					radioMenu3,
-					menu.Separator(),
-					radioMenu1,
-					radioMenu2,
-					radioMenu3,
-					&menu.MenuItem{
-						Label: "Hover over me",
-						Type:  menu.TextType,
-					},
-					menu.Text("Text Menu Item", nil, nil),
-				),
-			),
+			menu.Separator(), // <br />
 			menu.Text("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
 				runtime.Quit(a.ctx)
 			}),
 		)),
-		menu.SubMenu("Tests", menu.NewMenuFromItems(
-			menu.Text("Show secret menu...😉", nil, toggleSecretMenu),
-			menu.Separator(),
-			secretMenu,
-		),
-		),
 	)
 
 }
