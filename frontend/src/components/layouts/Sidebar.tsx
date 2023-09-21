81aebed068ad5c9c4f556805f26812cbf1b2f754
Naithar01 <eeung025@gmail.com>
2023-09-21 21:47:51 +0900

    * Sidebar Active 추가

--- frontend/src/components/layouts/Sidebar.tsx
+++ frontend/src/components/layouts/Sidebar.tsx
@@ -1,24 +1,46 @@
 import { Fragment, useState } from "react"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons"
 
 import "../../styles/common/sidebar_style.css"
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const ToggleSidebar = (): void => {
+    setIsOpen((prev) => {
+      return !prev
+    })
+    return 
+  }
+
   return (
     <Fragment>
       <nav className="sidebar_nav">
         <div className="sidebar_nav_toggle_icon">
-          <button className="sidebar_nav_toggle_btn" type="button">
+          <button className="sidebar_nav_toggle_btn" type="button" onClick={ToggleSidebar}>
             <FontAwesomeIcon icon={faBars} />
           </button>
         </div>
         <p className="sidebar_nav_logo">폴더 동기화</p>
       </nav>
-      <div className="sidebar">
-
+      <div className={`sidebar ${isOpen == true ? 'active' : ''}`}>
+        <div className="sidebar_header">
+          <h4>폴더 동기화</h4>
+          <button className="sidebar_toggle_btn" type="button" onClick={ToggleSidebar}>
+            <FontAwesomeIcon icon={faX}/>
+          </button>
+        </div>
+        <div className="sidebar_content">
+          <ul>
+            <li><a>Content 1</a></li>
+            <li><a>Content 2</a></li>
+            <li><a>Content 3</a></li>
+          </ul>
+        </div>
       </div>
+      <div className={`sidebar_overlay ${isOpen == true ? 'active' : ''}`} onClick={ToggleSidebar}></div>
     </Fragment>
   )
 }
