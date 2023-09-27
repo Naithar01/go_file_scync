39fe36f39193ad3d1f1735c8e20ce4a979e3f851
naithar01 <eeung025@gmail.com>
2023-09-28 07:59:20 +0900

    * grid를 사용하여 폴더 레이아웃 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -80,24 +80,24 @@ function App() {
 			: 
 			<Fragment>
         <div className="main">
-        <div className="folderStructure">
-          {resFileData && resFileData.length > 0 && resFileData.map((DirData) => {
-            const marginLeft = `${DirData.depth * 6}px`;
-            const paddingLeft = `${DirData.depth * 6}px`;
-            const verticalLineHeight = `${((resFileData.filter((fileStr) => fileStr.key.includes(DirData.key) && fileStr.key.startsWith(DirData.key))).length - 1) * 20 + 10}px`;
+          <div className="folderStructure">
+            {resFileData && resFileData.length > 0 && resFileData.map((DirData) => {
+              const marginLeft = `${DirData.depth * 6}px`;
+              const paddingLeft = `${DirData.depth * 6}px`;
+              const verticalLineHeight = `${((resFileData.filter((fileStr) => fileStr.key.includes(DirData.key) && fileStr.key.startsWith(DirData.key))).length - 1) * 20 + 10}px`;
 
-            return (
-              <div className="folder_wrap" key={DirData.key} style={{ marginLeft, paddingLeft }}>
-                {DirData.depth > 0 && (
-                  <div className="verticalLine" style={{ height: verticalLineHeight }}></div>
-                )}
-                <div className={`folder ${DirData.key}`}>
-                  <i className="folder_icon"></i>{DirData.key}
+              return (
+                <div className="folder_wrap" key={DirData.key} style={{ marginLeft, paddingLeft }}>
+                  {DirData.depth > 0 && (
+                    <div className="verticalLine" style={{ height: verticalLineHeight }}></div>
+                  )}
+                  <div className={`folder ${DirData.key}`}>
+                    <i className="folder_icon"></i>{DirData.key}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
           <div className="connect_folderStructure">
             연결 된 상대 PC 파일 정보들...
           </div>
