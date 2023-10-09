15dbacebc927d70b4c7e743fc86cde37bec4836b
naithar01 <eeung025@gmail.com>
2023-10-09 18:55:45 +0900

    * 폴더 구조 View 수정 * 폴더 및 파일의 Depth에 따라 트리 구조를 이루게 수정

--- frontend/src/components/directory/DirectoryList.tsx
+++ frontend/src/components/directory/DirectoryList.tsx
@@ -1,39 +1,67 @@
-import { RenameFileData } from "../../App"
-
-import { file } from "../../../wailsjs/go/models"
+import { RenameFileData } from "../../App";
+import { file } from "../../../wailsjs/go/models";
 
 type Props = {
-  resFileData: RenameFileData[]
-}
+  resFileData: RenameFileData[];
+};
 
-const Folder = ({ name, depth, files }: { name: string; depth: number, files: file.File[] }) => (
-  <div className="folder" style={{ marginLeft: `${depth * 10}px` }}>
-    {depth > 0 ? <span className="vertical-line">│</span> : <span style={{paddingLeft: 10}}></span>} 📁 {name}
-    { files.map((fileItem, index) => {
-      return (
-        fileItem.filename && <File key={index} name={fileItem.filename} depth={fileItem.depth} />
-      )
-    }) }
+//  "└─" : "├─"}
+// 폴더1/
+// ├─파일1.txt
+// ├─파일2.txt
+// └─하위폴더1/
+// ├─서브파일1.txt
+// └─서브파일2.txt
+// ├─하위 폴더2/
+// └─파일3.txt
+// └──하위 하위 폴더1/
+
+const Folder = ({
+  name,
+  depth,
+  files,
+  isLastDir,
+}: {
+  name: string;
+  depth: number;
+  files: file.File[];
+  isLastDir: boolean;
+}) => (
+  <div className="folder" >
+    {depth > 0 && (
+      <span className="vertical-line">{isLastDir ? "└" : "├"}</span>
+    )}
+    <span className="vertical-line">{"─".repeat(depth * 1)}</span>
+    📁 {name}
+    {files.map((fileItem, index) => (
+      fileItem.filename && <File
+        key={index}
+        name={fileItem.filename}
+        depth={depth + 1}
+        isLastFile={index === files.length - 1}
+      />
+    ))}
   </div>
 );
 
-const File = ({ name, depth }: { name: string; depth: number }) => (
-  <div className="file" style={{ marginLeft: `${depth * 10}px` }}>
-    {depth > 0 ? <span className="vertical-line">│</span> : <span style={{paddingLeft: 10}}></span>} 📄 {name}
+const File = ({ name, depth, isLastFile }: { name: string; depth: number; isLastFile: boolean }) => (
+  <div className="file" >
+    {depth > 0 && (
+      <span className="vertical-line">{isLastFile ? "└" : "├"}</span>
+    )}
+    <span className="vertical-line">{"─".repeat(depth * 1)}</span>
+    📄 {name}
   </div>
 );
 
 const DirectoryList = ({ resFileData }: Props) => {
   return (
-    <div>
-      {resFileData.map((dirItem, index) => {
-          return (
-            <Folder key={index} name={dirItem.key} depth={dirItem.depth} files={dirItem.files} />
-          )
-        })
-      };
+    <div className="folderStructure">
+      {resFileData.map((dirItem, index) => (
+        <Folder key={index} name={dirItem.key} depth={dirItem.depth} files={dirItem.files} isLastDir={index === resFileData.length - 1} />
+      ))}
     </div>
   );
 };
 
-export default DirectoryList
+export default DirectoryList;
