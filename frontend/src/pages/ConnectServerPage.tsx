d1a9f0aa7a084ceb78248ac359df3a6b980644a4
naithar01 <eeung025@gmail.com>
2023-10-14 09:19:59 +0900

    * 연결할 PC의 정보를 입력하는 페이지 디자인 수정 * 연결할 PC의 포트를 입력하는 State 추가 * State를 관리 해주는 Handler 함수 추가

--- frontend/src/pages/ConnectServerPage.tsx
+++ frontend/src/pages/ConnectServerPage.tsx
@@ -1,9 +1,31 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
+
+import Alert from "../components/common/Alert"
+
+import "../styles/pages/connect_server_page_style.css"
 
 const ConnectServerPage = () => {
+  const [portState, setPortState] = useState<number>()
+
+  const ChangePortStateHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const enteredPort: number = +e.target.value;
+  
+    if (!Number.isNaN(enteredPort)) setPortState(() => enteredPort);
+  }
+
   return (
     <Fragment>
-      Connect Server Page
+      <div className="connect_server_page">
+        <Alert text="연결할 PC의 포트를 입력하세요." />
+      </div>
+      <div className="connect_server_page_port_inp_areas">
+        <div className="connect_server_page_port_inp_area">
+          <input type="text" inputMode="numeric" value={portState} placeholder="포트를 입력하세요." onChange={ChangePortStateHandler}/>
+        </div>
+        <div className="connect_server_page_port_inp_area">
+          <button type="button" onClick={() => {}}>서버 연결</button>
+        </div>
+      </div>
     </Fragment>
   )
 }
